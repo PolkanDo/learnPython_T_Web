039255fa3145a368601d7250d28939e25f123e13
Pavel <mrmarmon@mail.ru>
2020-09-01 22:46:31 +0300

    Added HTML doc

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
 
 app = Flask(__name__)
@@ -6,11 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    title = 'Weather'
     weather = weather_by_city('Moscow,Russia')
-    if weather:
-        return f'Weather: {weather["temp_C"]}, feels like {weather["FeelsLikeC"]}'
-    else:
-        return "Weather server is temporarily unavailable..."
+    return render_template('index.html', page_title=title, weather=weather)
 
 if __name__ == "__main__":
     app.run(debug=True)
