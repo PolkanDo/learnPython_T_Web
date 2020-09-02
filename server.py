7c2ec88eba50dd75e48185bdf758c497daebe35b
Pavel <mrmarmon@mail.ru>
2020-09-02 23:55:50 +0300

    Added python_org parser

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from weather import weather_by_city
+from python_org_news import get_python_news
 
 app = Flask(__name__)
 
@@ -8,7 +9,8 @@ app = Flask(__name__)
 def index():
     title = 'Python News'
     weather = weather_by_city('Moscow,Russia')
-    return render_template('index.html', page_title=title, weather=weather)
+    news_list = get_python_news()
+    return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
